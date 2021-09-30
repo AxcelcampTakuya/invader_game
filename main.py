66041c7c516713c9c172207e1c9574e67d701a51
AxcelcampTakuya <axcelcamp.takuya@gmail.com>
2021-09-30 11:10:25 +0900

    9月29日

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ from pygame.locals import *
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
-screen.fill((0,0,0))
 pygame.display.set_caption("インベーダーゲーム")
+screen.fill((0,0,0))
 
+player = pygame.image.load("player.png")
+# enemy = pygame.imageload("enemy.png")
+
+playerX = 200
+playerY = 100
+
+# enemyX = 100
+# enemyY = 200
 
 running = True
 
 # 特になし。強いて言うなら吉田力という男性がうざかった
 while running:
+    screen.blit(player,(playerX,playerY))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 screen.fill((255,0,0))
@@ -24,4 +34,3 @@ while running:
             if event.key == K_DOWN:
                 screen.fill((255,255,0))
     pygame.display.update()
-
